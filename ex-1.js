7455ed273eca3be09a3496e8b33f73e1b9519eca
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-05-10 13:04:59 +0700

    feat(js): add answer ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 // Exercise #1: Food Order
+const foodOrder = {};
+
+foodOrder.orderNumber = 'A0234';
+foodOrder.address = 'Bangkok';
+foodOrder.paymentType = 'Cash';
+foodOrder.totalPrice = 4500;
+foodOrder.restuarantName = 'MK';
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = 'Credit Card';
+
+console.log(foodOrder);
 
 // Start coding here
